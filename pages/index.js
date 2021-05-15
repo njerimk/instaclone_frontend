52968595b7060530c1d4b377860c0b0f91d97ace
njerimk <nmurugikamau@gmail.com>
2021-05-15 18:03:29 -0400

    adding firebase for front end

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <div>Welcome to Next.js!</div>
+export default function Home(props) {
+  return (
+  <> 
+    {props.ssrWorking ? (
+    <div>
+      <img src="/assets/success.jpg" height="500" />
+      <h2> Deployment Successful of Nextjs Application with SSR on Firebase. </h2>
+    </div>
 
+    ) : (
+    <h2>SSR not working</h2>
+    )}
+  </>
+  )
 }
+
+export async function getServerSideProps() {
+  return { props: {ssrWorking: true }};
+}
+  
